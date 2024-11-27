9a95692109e8b18d511b9476ccea20b809c839bb
Victor Martin Aguado <v.martinaguado@globant.com>
2024-11-27 13:35:49 +0100

    cambios chapter 17

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -60,7 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
     }catch(error){
         return{
-            message: 'Database Error: Failed to Create Invoice.',
+            message: 'Database Error: Failed to Create Invoice.', error,
         };
     }
     
@@ -94,7 +94,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
     `;
     }catch(error){
         return{
-            message: 'Database Error: Failed to Update Invoice.',
+            message: 'Database Error: Failed to Update Invoice.', error,
         };
     }
    
@@ -110,7 +110,7 @@ export async function deleteInvoice(id: string) {
         return { message: 'Deleted Invoice.' };
     }catch(error){
         return{
-            message: 'Database Error: Failed to Delete Invoice.',
+            message: 'Database Error: Failed to Delete Invoice.', error,
         };
     }
 }
